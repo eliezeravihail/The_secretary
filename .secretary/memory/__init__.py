6e1871f6d99e6fbc189fccac437355981e355d1e
eliezer avihail <48094022+eliezeravihail@users.noreply.github.com>
2026-05-28 15:27:07 +0300

    revert: remove memory engine (not part of the secretary system)

--- .secretary/memory/__init__.py
+++
@@ -1,7 +0,0 @@
-from .store import MemoryStore
-from .embed import get_embedder
-from .chunker import get_chunker
-from .retriever import Retriever
-from .indexer import Indexer
-
-__all__ = ["MemoryStore", "get_embedder", "get_chunker", "Retriever", "Indexer"]
